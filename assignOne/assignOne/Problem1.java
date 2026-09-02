581765d480b81445f9080316197108bf80117319
Sebastian Lockhart <slockh00@uafs.edu>
2026-09-01 19:42:54 -0500

    comment

--- assignOne/assignOne/Problem1.java
+++ assignOne/assignOne/Problem1.java
@@ -3,14 +3,34 @@ package assignOne;
 public class Problem1 {
 
 	public static void main(String[] args) {
-		
+		System.out.println(stringToInt("+159263"));
 
 	}
 	
 	public static int stringToInt(String str) {
+		int number = 0;
+		int timesTen = 1;
+		int p;
+		int i;
+		for (p = str.length()-1; p > 1; p--) {
+			timesTen *= 10;
+		}
 		
-		
-		return 0;
+		for (i = 0; i < str.length(); i++) {
+			if ((int) str.charAt(i) <= 57 && (int) str.charAt(i) >= 48) {
+				int temp = ((int) str.charAt(i) - 48);
+				
+				number += temp*timesTen;
+				timesTen/=10;
+			}
+			
+		}
+		if (str.charAt(0) == '-' ) {
+			return -1*number;
+		}
+		else {
+			return number;
+		}
 	}
 
 }
--- assignOne/assignOne/Problem2.java
+++ assignOne/assignOne/Problem2.java
@@ -3,14 +3,14 @@ package assignOne;
 public class Problem2 {
 
 	public static void main(String[] args) {
-		
+		System.out.println(intToString(895168719));
 
 	}
 	
 	public static String intToString(int value) {
-		
-		
-		return null;
+		String result = "";
+		result += value;
+		return result;
 	}
 
 }
